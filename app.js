51fa06bcaaa0c3e421be6449a8061dd5b176d052
Enea Guidi <eneag3@hotmail.com>
2020-02-25 21:50:08 +0100

    Added table project, populated through GitHub API

--- app.js
+++ app.js
@@ -1,33 +1,53 @@
 //Get the needed object from HTML
 const sections = document.querySelectorAll('section');
 const focus = document.querySelector('.focus');
+const projects_table = document.querySelector(".projects_table");
 
-//Initialize options to animate the NavNar
+//Initialize options to animate the NavBar
 const options = { threshold: 0.7 };
 
 let observer = new IntersectionObserver(navCheck, options);
 
 function navCheck(entries) {
     entries.forEach(entry => {
+        //Get the name of the  active Section, then get the respective anchor in the NavBar, then the anchor coords
         const className = entry.target.className;
         const activeAnchor = document.querySelector(`[data-page=${className}]`);
         const coords = activeAnchor.getBoundingClientRect();
-        const directions = {
-            height: coords.height,
-            width: coords.width,
-            top: coords.top,
-            left: coords.left
-        };
-
-        if (entry.isIntersecting) { //Interpolates the gradients to determine the "focus" color
-            focus.style.setProperty("height", `${directions.height}px`);
-            focus.style.setProperty("width", `${directions.width}px`);
-            focus.style.setProperty("top", `${directions.top}px`);
-            focus.style.setProperty("left", `${directions.left}px`);
+        
+        /*Then if we're currently seeing that section (there's intersection), sets the focus 
+        (the colored "bookmark" in the navbar) coords to the currently active anchor coords*/
+        if (entry.isIntersecting) { 
+            focus.style.setProperty("height", `${coords.height}px`);
+            focus.style.setProperty("width", `${coords.width}px`);
+            focus.style.setProperty("top", `${coords.top}px`);
+            focus.style.setProperty("left", `${coords.left}px`);
         }
     });
 }
 
+async function populateTable() {
+    //Url that will eventually return my repo, sorted from the last to the most recent updated
+    const API_request = "https://api.github.com/search/repositories?q=user:its-hmny&sort=updated";
+    response = await fetch(API_request);
+    result = await response.json(); //JSON parsing
+    
+    result.items.forEach(repo => {
+        const position = result.items.left; //Position in which the repo elements has to be inserted
+        let newRow = projects_table.insertRow(position);
+        
+        //Adds the needed repo's info to the table row element
+        newRow.insertCell().append(document.createTextNode(repo.name));
+        newRow.insertCell().append(document.createTextNode(repo.description));
+        newRow.insertCell().append(document.createTextNode(repo.language));
+    });
+}
+
+
+populateTable();
+
+/*This little subroutine cycles throughall the section and pass each of them to 
+the observer that will call the navCheck method */
 sections.forEach(section => {
     observer.observe(section);
 });
