9e9275730c3e4bd11746d7753fc96a37d168ac43
Enea Guidi <guidienea@gmail.com>
2023-11-07 23:04:08 +0000

    feat(landing): Initial draft (skeleton + copy text)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import LandingSection from './components/LandingSection';
+
 export default function App() {
   return (
     <>
-      <h2 className="text-3xl font-bold underline">
-        Click on the Vite and React logos to learn more
-      </h2>
+      <LandingSection />
+      <LandingSection />
+      <LandingSection />
     </>
   );
 }
