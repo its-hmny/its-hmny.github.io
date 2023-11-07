bd99204c96321f2cf8a061d29195d67099eb134b
Enea Guidi <guidienea@gmail.com>
2023-11-07 21:33:16 +0000

    chore(vscode): Init settings.json for VS Code

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+
+  // Plugin to sort the Tailwind CSS classnames
+  plugins: ['prettier-plugin-tailwindcss'],
 };
