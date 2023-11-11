bf62358fd2c04c604ced363b08d6c09c43fda98a
Enea Guidi <guidienea@gmail.com>
2023-11-11 01:04:07 +0000

    style(landing): Minimal responsiveness + touch ups

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -1,15 +1,16 @@
 export default function LandingSection() {
   return (
-    <div className='flex h-screen items-center justify-evenly bg-black align-middle text-white'>
-      <div className='w-1/3'>
-        <img src='http://picsum.photos/200' className='h-full w-full rounded-full object-cover' />
+    <div className='z-20 flex h-screen items-center justify-evenly bg-black align-middle text-white max-lg:w-screen	max-lg:flex-col'>
+      <div className='h-screen w-1/2 max-lg:h-2/5 max-lg:w-screen'>
       </div>
 
-      <div className='w-1/2 p-8'>
+      <div className='w-1/2 p-8 max-lg:w-screen'>
         <div className='mb-3'>
-          <h1 className='mb-3 text-4xl font-bold'>👋 Hi there! I'm Enea</h1>
+          <h1 className='mb-3 text-4xl font-bold'>
+            👋 Hi there! I'm <span className='text-lime-400'>Enea</span>
+          </h1>
           <h4 className='font-italic mb-3 text-2xl text-orange-400'>Fullstack Developer & Blockchain Specialist</h4>
-          <p className='text-lg text-gray-500'>
+          <p className='text-justify text-lg text-gray-500'>
             I architect and deliver unique digital experiences on the Frontend while implementing robust and extensible
             programs the Backend. In order to deliver a reliable solution in the least amount of time I adopt already
             established tech stacks and mix it with some of the most cutting-edge solutions available on the market.
@@ -24,7 +25,9 @@ export default function LandingSection() {
         </div>
 
         <div className='mb-4 mt-8 '>
-          <button className='rounded-full bg-orange-400 px-4 py-2 text-lg text-white'>💼 See some of my work</button>
+          <button onClick={alert} className='rounded-full bg-orange-400 px-4 py-2 text-lg text-white'>
+            💼 See some of my work
+          </button>
         </div>
       </div>
     </div>
