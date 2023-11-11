147b596f53e4e8fab202eff107a318bbc92952f9
Enea Guidi <guidienea@gmail.com>
2023-11-11 01:06:09 +0000

    feat(landing): Animated Macbok 3D model on landing section
    The following implementation has been heavily inspired by what @pmndrs
    showcased in one of ts examples on R3F docs (https://docs.pmnd.rs/).
    Even the 3D model has been downloaded from said example since I don't
    have any idea whatsoever on ow to create one from scratch.
    
    Credits:
    - R3F Documentation Site: https://docs.pmnd.rs/
    - The inspirational work: https://codesandbox.io/s/q23sw
    - The author of both:     https://twitter.com/pmndrs

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -1,7 +1,27 @@
+import { Environment, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+
+import MacBook from './MacbookAir';
+
 export default function LandingSection() {
   return (
     <div className='z-20 flex h-screen items-center justify-evenly bg-black align-middle text-white max-lg:w-screen	max-lg:flex-col'>
       <div className='h-screen w-1/2 max-lg:h-2/5 max-lg:w-screen'>
+        <Canvas camera={{ position: [-5, 0, -23], fov: 55 }} style={{ width: '100%', height: '100%' }}>
+          <pointLight position={[10, 10, 10]} intensity={1.5} />
+          <Suspense fallback={null}>
+            <group rotation={[0, Math.PI, 0]} position={[0, 1, 0]}>
+              <MacBook />
+            </group>
+            <Environment preset='city' />
+          </Suspense>
+          {/*
+            'ContactShadow' are not needed for the time being since the background is just black 
+            <ContactShadows position={[0, -4.5, 0]} opacity={0.4} scale={20} blur={1.75} far={4.5} />
+          */}
+          <OrbitControls enableZoom={true} enablePan={false} />
+        </Canvas>
       </div>
 
       <div className='w-1/2 p-8 max-lg:w-screen'>
@@ -15,12 +35,11 @@ export default function LandingSection() {
             programs the Backend. In order to deliver a reliable solution in the least amount of time I adopt already
             established tech stacks and mix it with some of the most cutting-edge solutions available on the market.
             {/* TODO (Enea): Integrate the one above w/ this one
-            <br />
             As a developer, my skill set spans a broad spectrum, showcasing adaptability and versatility. From delving
             into the intricacies of blockchain technology to exploring the creative realms of 3D development, I bring a
             diverse range of expertise to the table. Professionally navigating both established and cutting-edge
             frameworks, I am committed to crafting solutions that seamlessly blend innovation with reliability.
-            */}
+          */}
           </p>
         </div>
 
