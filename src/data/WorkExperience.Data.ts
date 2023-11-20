42334ae5b284913467f9d9f89baa21039147bd3f
Enea Guidi <guidienea@gmail.com>
2023-11-20 23:55:26 +0000

    feat(work): Added real work timeline + minor touch-ups

---
+++ src/data/WorkExperience.Data.ts
@@ -0,0 +1,34 @@
+import { Bitcoin, ServerCog, Store } from 'lucide-react';
+
+// TODO(hmny): Would be nice to use LinkedIn APIs and populate this content dynamically
+export default [
+  {
+    icon: Bitcoin,
+    role: 'Backend Developer',
+    period: ['October 2022', 'Present'],
+    company: { name: 'BKN301', website: 'https://bkn301.com/' },
+    skills: ['Golang', 'C#', 'Python', 'Stellar', 'Docker', 'Terraform'],
+    description:
+      "Designed and implemented '301chain': a scalable Closed Payment Circuit (CPC), based on Stellar blockchain, capable of handling 2+ Mln dollars in daily transactions. Contributed to the design and development of BKN301's core banking system ('301core') as well as the backend of '301wallet', the company instant card issuing product.",
+  },
+
+  {
+    icon: ServerCog,
+    role: 'Fullstack Developer',
+    period: ['February 2022', 'October 2022'],
+    company: { name: 'Moonia', website: 'https://www.moonia.it/' },
+    skills: ['Solidity', 'Typescript', 'Ethereum', 'Solana', 'Hardhat'],
+    description:
+      "Developed, tested and released two NFT collections on both Ethereum and Solana with special focus on maximizing cost efficiency. Designed and developed the MVP for 'Moonium': a SaaS product that aimed to simplify the management, discovery and interaction of Web3 communities (especially the ones spanning across multiple blockchains).",
+  },
+
+  {
+    icon: Store,
+    role: 'Fullstack Developer',
+    period: ['March 2021', 'October 2022'],
+    company: { name: 'Botika', website: 'https://botika.ai/' },
+    skills: ['Golang', 'Typescript', 'Next.js', 'Nest.js', 'Python'],
+    description:
+      "Developed 'Meeters', a webapp for the organization, management and participation to virtual fairs/expositions during the Covid-19 pandemic. The platform has been used by national and international fairs with up-to 7.000+ online simultaneous users.",
+  },
+] as const;
--- src/views/Work.View.tsx
+++ src/views/Work.View.tsx
@@ -1,4 +1,8 @@
-import { Bitcoin, LibraryBig, Store } from 'lucide-react';
+import { OrbitControls, TorusKnot } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { MeshStandardMaterial } from 'three';
+
+import WorkExperienceData from '../data/WorkExperience.Data';
 
 /* 
  This view is the second section of my portfolio, as other sections of the website it's also split in two section:
@@ -11,7 +15,7 @@ export default function WorkSection() {
   return (
     <section className='flex h-screen w-screen flex-row items-center justify-evenly bg-black align-middle text-white'>
       {/* First half of the page */}
-      <div className='h-full w-1/2 p-10'>
+      <div className='h-full w-6/12 p-10'>
         <h3 className='font-italic mb-3 text-2xl text-orange-400'>My work experience</h3>
         <p className='text-md mb-10 text-justify text-gray-500'>
           Don't let my young age fool you! In my previous work experiences I was fortunate enough to explore and broaden
@@ -21,56 +25,35 @@ export default function WorkSection() {
 
         {/* Past work experiences timeline */}
         <ol className='relative border-s border-gray-200 dark:border-orange-400'>
+          {WorkExperienceData.map(item => (
           <li className='mb-10 ms-4'>
             <div className='absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-orange-400' />
             <time className='mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
-              October 2022 - Present
+                {item.period[0]} - {item.period[1]}
             </time>
             <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
-              <Bitcoin className='mr-1 inline' />
-              Fullstack Developer @ BKN301
+                <item.icon className='mr-1 inline' />
+                {item.role} @{' '}
+                <a href={item.company.website} target='_blank' className='text-orange-500 hover:underline'>
+                  {item.company.name}
+                </a>
             </h3>
             <p className='mb-4 text-base font-normal text-gray-500 dark:text-gray-400'>
-              Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order
-              E-commerce & Marketing pages. Get access to over 20+ pages including a dashboard layout, charts, kanban
-              board, calendar, and pre-order E-commerce & Marketing pages. Get access to over 20+ pages including a
-              dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.
+                {item.description}
               <br />
-              <span className='inline-flex items-center rounded-full bg-orange-100 px-2.5 py-0.5 text-xs font-medium text-orange-600 dark:bg-orange-700 dark:text-orange-300'>
-                <span className='me-1 h-2 w-2 rounded-full bg-orange-500' />
-                Available
+                {item.skills.map(skill => (
+                  <span className='mr-2 inline-flex items-center rounded-full bg-lime-100 px-2.5 py-0.5 text-xs font-medium text-lime-600 dark:bg-lime-700 dark:text-lime-300'>
+                    <span className='me-1 h-2 w-2 rounded-full bg-lime-500' />
+                    {skill}
               </span>
+                ))}
             </p>
           </li>
-          <li className='mb-10 ms-4'>
-            <div className='absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-orange-400' />
-            <time className='mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
-              February 2021 - October 2022
-            </time>
-            <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
-              <Store className='mr-1 inline' /> Fullstack Developer @ Botika
-            </h3>
-            <p className='text-base font-normal text-gray-500 dark:text-gray-400'>
-              All of the pages and components are first designed in Figma and we keep a parity between the two versions
-              even as we update the project.
-            </p>
-          </li>
-          <li className='ms-4'>
-            <div className='absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-orange-400' />
-            <time className='mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
-              September 2018 - March 2022
-            </time>
-            <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
-              <LibraryBig className='mr-1 inline' />
-              BSc in Computer Science @ UniBo
-            </h3>
-            <p className='text-base font-normal text-gray-500 dark:text-gray-400'>
-              Get started with dozens of web components and interactive elements built on top of Tailwind CSS.
-            </p>
-          </li>
+          ))}
         </ol>
       </div>
-      <div className='h-full w-1/2'></div>
+      <div className='h-full w-5/12'>
+      </div>
     </section>
   );
 }
