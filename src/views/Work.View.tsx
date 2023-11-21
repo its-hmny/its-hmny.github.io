912e10013f5cf708345ac90f36481c9724770ad4
Enea Guidi <guidienea@gmail.com>
2023-11-21 22:11:08 +0000

    feat(landing): Animated NonEuclideanBox model on work section
    The following implementation has been heavily inspired by what @pmndrs
    showcased in one of ts examples on R3F docs (https://docs.pmnd.rs/).
    Even the 3D model has been downloaded from said example since I don't
    have any idea whatsoever on ow to create one from scratch.
    
    Credits:
        - R3F Documentation Site: https://docs.pmnd.rs/
        - The inspirational work: https://codesandbox.io/s/drc6qg
        - The author of both:     https://twitter.com/pmndrs

--- src/views/Work.View.tsx
+++ src/views/Work.View.tsx
@@ -1,8 +1,10 @@
-import { OrbitControls, TorusKnot } from '@react-three/drei';
+import { CameraControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { MeshStandardMaterial } from 'three';
+import { Suspense } from 'react';
 
 import WorkExperienceData from '../data/WorkExperience.Data';
+import NonEuclideanBox from '../models/NonEuclideanBox.Model';
+import Spinner from '../models/Spinner.Model';
 
 /* 
  This view is the second section of my portfolio, as other sections of the website it's also split in two section:
@@ -26,33 +28,41 @@ export default function WorkSection() {
         {/* Past work experiences timeline */}
         <ol className='relative border-s border-gray-200 dark:border-orange-400'>
           {WorkExperienceData.map(item => (
-          <li className='mb-10 ms-4'>
-            <div className='absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-orange-400' />
-            <time className='mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
+            <li className='mb-10 ms-4'>
+              <div className='absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-orange-400' />
+              <time className='mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
                 {item.period[0]} - {item.period[1]}
-            </time>
-            <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
+              </time>
+              <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
                 <item.icon className='mr-1 inline' />
                 {item.role} @{' '}
                 <a href={item.company.website} target='_blank' className='text-orange-500 hover:underline'>
                   {item.company.name}
                 </a>
-            </h3>
-            <p className='mb-4 text-base font-normal text-gray-500 dark:text-gray-400'>
+              </h3>
+              <p className='mb-4 text-base font-normal text-gray-500 dark:text-gray-400'>
                 {item.description}
-              <br />
+                <br />
                 {item.skills.map(skill => (
                   <span className='mr-2 inline-flex items-center rounded-full bg-lime-100 px-2.5 py-0.5 text-xs font-medium text-lime-600 dark:bg-lime-700 dark:text-lime-300'>
                     <span className='me-1 h-2 w-2 rounded-full bg-lime-500' />
                     {skill}
-              </span>
+                  </span>
                 ))}
-            </p>
-          </li>
+              </p>
+            </li>
           ))}
         </ol>
       </div>
+
+      {/* Second half of the page with the 3D model */}
       <div className='h-full w-5/12'>
+        <Canvas shadows camera={{ position: [-3, 0.5, 3] }} style={{ width: '100%', height: '100%' }}>
+          <CameraControls makeDefault />
+          <Suspense fallback={<Spinner />}>
+            <NonEuclideanBox />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
