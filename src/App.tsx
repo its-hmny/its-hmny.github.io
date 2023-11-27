df9654d9e11b7b91d997f29a5b778672646a481f
Enea Guidi <guidienea@gmail.com>
2023-11-27 22:37:03 +0000

    style(all): Minor improvements to UI/UX

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Splashscreen from './components/Splashscreen.tsx';
 
 import LandingSection from './views/LandingSection.tsx';
+import SocialSection from './views/SocialMedia.tsx';
 import WorkSection from './views/WorkSection.tsx';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <Splashscreen>
       <LandingSection />
       <WorkSection />
+      <SocialSection />
     </Splashscreen>
   );
 }
