cb9b7e36e8d68516a7ebca6cca7161582cf57954
Enea Guidi <guidienea@gmail.com>
2024-01-09 21:21:00 +0000

    style(all): Improved and simplified 'Splashscreen.tsx'

--- src/components/Splashscreen.tsx
+++ src/components/Splashscreen.tsx
@@ -1,8 +1,6 @@
 import { useProgress } from '@react-three/drei';
 import { ReactNode, Suspense } from 'react';
 
-import './Splashscreen.css';
-
 type SplashscreenProps = { children: ReactNode };
 
 export default function Splashscreen({ children }: SplashscreenProps) {
@@ -13,16 +11,9 @@ export default function Splashscreen({ children }: SplashscreenProps) {
       {progress < 100 ? (
         // Overlay component with full width and height that will 'hide' the real content behind
         <div className='z-1 absolute left-0 top-0 flex h-screen w-screen items-center justify-center bg-black'>
-          <div className='pl'>
-            <div className='pl__outer-ring'></div>
-            <div className='pl__inner-ring'></div>
-            <div className='pl__track-cover'></div>
-            <div className='pl__ball'>
-              <div className='pl__ball-texture'></div>
-              <div className='pl__ball-outer-shadow'></div>
-              <div className='pl__ball-inner-shadow'></div>
-              <div className='pl__ball-side-shadows'></div>
-            </div>
+          <div className='h-[250px] w-[250px] rounded-full border-8 border-solid border-gray-300'>
+            <div className='circle animate-spin-fast absolute h-10 w-10 rounded-full bg-lime-500' />
+            <div className='circle animate-spin-slow absolute h-10 w-10 rounded-full bg-orange-500' />
           </div>
         </div>
       ) : null}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      keyframes: { circle: { to: { 'offset-distance': '100%' } } },
+      animation: {
+        'spin-slow': 'circle 5s linear infinite',
+        'spin-fast': 'circle 3s linear infinite',
+      },
+    },
+  },
 
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
