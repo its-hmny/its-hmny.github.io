1583995dea1a6d44c3ac0baba42d3eac0500863e
Enea Guidi <guidienea@gmail.com>
2024-05-27 21:48:10 +0200

    build(cd): Amended CD pipeline and Next option to support SSG + Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+const config = {
+  output: 'export', // Opt-in to static site genration (since we deploy on GitHub pages)
 
-export default nextConfig;
+  // ! A must do operation when having <Suspense /> boundaries in a static generated site (SSG)
+  // ! https://nextjs.org/docs/app/building-your-application/deploying#streaming-and-suspense
+  headers: async () => [{ source: '/:path*{/}?', headers: [{ key: 'X-Accel-Buffering', value: 'no' }] }],
+};
+
+export default config;
