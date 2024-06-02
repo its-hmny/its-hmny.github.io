356ca3469da1648c5dcd83ee2baa750dc8ff0326
Enea Guidi <guidienea@gmail.com>
2024-06-02 23:08:53 +0200

    feat(all): Unified theming through 'tailwind.config.ts'
    This will enable in future also to generate a new theme at
    random and apply it on page load as whell as moving on and
    off to different themes
    in just a few minutes changes.

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 export function Title({ job }: { job: Job }) {
   return (
-    <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
+    <h3 className='text-theme_fg text-lg font-semibold'>
       <job.icon className='mr-1 inline' />
       {job.role} @{' '}
-      <Link href={job.company.website} target='_blank' className='text-orange-500 hover:underline'>
+      <Link href={job.company.website} target='_blank' className='text-theme_primary-500 hover:underline'>
         {job.company.name}
       </Link>
     </h3>
@@ -15,15 +15,15 @@ export function Title({ job }: { job: Job }) {
 
 export function Description({ job }: { job: Job }) {
   return (
-    <div className='mt-2 text-base font-normal text-gray-500 dark:text-gray-400'>
+    <div className='text-theme_fg_accent mt-2 text-base font-normal'>
       <p>{job.description}</p>
       <div className='max-sm:mt-1 max-sm:flex max-sm:flex-row max-sm:flex-wrap max-sm:justify-evenly'>
         {job.skills.map(skill => (
           <span
             key={skill}
-            className='mr-2 inline-flex items-center rounded-full bg-lime-100 px-2.5 py-0.5 text-xs font-medium text-lime-600 max-lg:m-[8px] dark:bg-lime-700 dark:text-lime-300'
+            className='bg-theme_secondary-700 text-theme_secondary-300 mr-2 inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium max-lg:m-[8px]'
           >
-            <span className='me-1 h-2 w-2 rounded-full bg-lime-500' />
+            <span className='bg-theme_secondary-500 me-1 h-2 w-2 rounded-full' />
             {skill}
           </span>
         ))}
--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,9 +1,9 @@
 export default function Spinner() {
   return (
-    <div className='flex h-full w-full items-center justify-center bg-black'>
-      <div className='h-[250px] w-[250px] rounded-full border-8 border-solid border-gray-300'>
-        <div className='circle h-10 w-10 animate-spin-fast rounded-full bg-lime-500' />
-        <div className='circle h-10 w-10 animate-spin-slow rounded-full bg-orange-500' />
+    <div className='bg-theme_bg flex h-full w-full items-center justify-center'>
+      <div className='border-theme_fg h-[250px] w-[250px] rounded-full border-8 border-solid'>
+        <div className='circle bg-theme_secondary-500 h-10 w-10 animate-spin-fast rounded-full' />
+        <div className='circle bg-theme_primary-500 h-10 w-10 animate-spin-slow rounded-full' />
       </div>
     </div>
   );
