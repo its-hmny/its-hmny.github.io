6908a1c6daf685c594e01fb0b0b37fd2a56be87c
Enea Guidi <guidienea@gmail.com>
2024-06-27 23:28:35 +0200

    chore(all): Initial metadata generation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,23 @@ const inter = Ubuntu({ weight: '400', subsets: ['latin'] });
 // TODO(hmny): Add full metadata generation, per-route basis
 // TODO(hmny): Check support for 'vercel/og' with static exports
 export const metadata: Metadata = {
-  title: 'Enea Guidi',
-  icons: '/PenroseTriangle.png',
-  description: 'TODO as well',
-  // openGraph: { images: [HOME_OG_IMAGE_URL] },
+  creator: 'Enea Guidi',
+  title: 'Enea Guidi (hmny)',
+  icons: [{ rel: 'icon', url: '/PenroseTriangle.png' }],
+  authors: [{ name: 'Enea Guidi', url: 'https://github.com/its-hmny' }],
+  keywords: ['portfolio', 'fullstack', 'developer', '3d', 'threejs', 'react', 'nextjs'],
+  description:
+    'Showcase of the work by Enea Guidi, a skilled Fullstack Developer and Blockchain Specialist. Explore reliable, fast, and cutting-edge applications built for various clients using the latest technologies.',
+
+  themeColor: 'dark',
+  generator: 'Next.js',
+  publisher: 'Enea Guidi',
+  manifest: '/manifest.json',
+  robots: { index: true, follow: true },
+  applicationName: 'My personal website',
+  alternates: { canonical: 'https://its-hmny.github.io' },
+
+  // openGraph: { images: [HOME_OG_IMAGE_URL] },s
 };
 
 type Props = Readonly<{ children: React.ReactNode }>;
