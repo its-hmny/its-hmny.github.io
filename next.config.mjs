af2d6c9dbba4b70bec04dcc386e4ba16757f3dff
Enea Guidi <guidienea@gmail.com>
2024-10-15 21:14:10 +0200

    fix(cd): Tried to fix strange issue w/ Next.js static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 import withMDX from '@next/mdx';
 
+const mdx = withMDX();
+
 /** @type {import('next').NextConfig} */
-const config = withMDX({
+const config = {
   output: 'export', // Opt-in to static site genration (since we deploy on GitHub pages)
 
-  // ? Configure `pageExtensions` to include MDX files
-  // ? pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 
   // ! A must do operation when having <Suspense /> boundaries in a static generated site (SSG)
   // ! https://nextjs.org/docs/app/building-your-application/deploying#streaming-and-suspense
@@ -15,6 +17,6 @@ const config = withMDX({
       headers: [{ key: 'X-Accel-Buffering', value: 'no' }],
     },
   ],
-});
+};
 
-export default config;
+export default mdx(config);
